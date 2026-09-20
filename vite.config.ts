c9a85aa40585ff4838ad093679cb22aab53351b8
Hien Pham <phhien203@gmail.com>
2026-09-20 11:04:12 +0300

    chore: update dependencies and refactor routing configuration
    - Added @react-router/remix-routes-option-adapter to devDependencies.
    - Updated remix-flat-routes to version 0.8.5.
    - Removed flatRoutes usage from vite.config.ts to prevent default remix routing.
    - Created app/routes.ts to define routes using remixRoutesOptionAdapter and flatRoutes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { reactRouter } from '@react-router/dev/vite';
-import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -10,12 +9,6 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-      },
-      // ignore all files in routes folder to prevent
-      // default remix convention from picking up routes
-      ignoredRouteFiles: ['**/.*'],
-      routes: async (defineRoutes) => {
-        return flatRoutes('routes', defineRoutes)
       },
     }),
     tsconfigPaths(),
