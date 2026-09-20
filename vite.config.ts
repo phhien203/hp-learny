e93a5f82035947234fc7011ef0a2f0e582ed9f6e
Hien Pham <phhien203@gmail.com>
2026-09-20 11:07:01 +0300

    chore: update .gitignore and tsconfig for react-router integration; add react-router.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { reactRouter } from '@react-router/dev/vite';
+import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [
-    reactRouter({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
 })
